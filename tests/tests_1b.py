3a61595aea92f37eb5b2311d9feadb6c77b41a74
Preebg <preebg09@gmail.com>
2026-03-29 16:50:35 -0400

    Update tests_1b.py

--- tests/tests_1b.py
+++ tests/tests_1b.py
@@ -14,6 +14,7 @@ def test_addition():
 
 def test_subtraction():
     assert simple_calculator("subtract", 5, 3) == 2     # Test for positive numbers
+    assert simple_calculator("subtract", 5, -5) == 10   # Test for positive and negative number
     assert simple_calculator("subtract", -2, -2) == 0   # Test for negative numbers
     assert simple_calculator("subtract", 0, 5) == -5    # Test for zero minuend
 
@@ -25,6 +26,7 @@ def test_multiplication():
 def test_division():
     assert simple_calculator("divide", 6, 3) == 2       # Test for positive numbers
     assert simple_calculator("divide", -4, 2) == -2     # Test for negative and positive number
+    assert simple_calculator("divide", -5, -5) == 1      # Test for zero numerator
     assert simple_calculator("divide", 5, 2) == 2.5     # Test for division resulting in float
 
 def test_division_by_zero():
